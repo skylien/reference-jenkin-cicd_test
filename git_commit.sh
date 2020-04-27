74589528a0b2b5e445536523b83b5fdb152640c3
skyline <skyline27042012@gmail.com>
2020-04-27 07:16:47 +0000

    first commit

---
+++ git_commit.sh
@@ -0,0 +1,9 @@
+git config --global user.name "skyline"
+git config --global user.email "skyline27042012@gmail.com"
+#cho "# reference-jenkin-cicd" >> README.md
+git init
+git add --all
+git commit -m "first commit"
+git remote add origin https://github.com/skylien/reference-jenkin-cicd_test.git
+git push -u origin master
+
